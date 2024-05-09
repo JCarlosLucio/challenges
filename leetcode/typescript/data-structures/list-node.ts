813abb61915b914200bd04b07395980467158d1e
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-05-08 22:28:19 -0600

    Add ListNode and createList

---
+++ leetcode/typescript/data-structures/list-node.ts
@@ -0,0 +1,20 @@
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function createList(arr: number[]): ListNode {
+  const root = new ListNode(arr.shift());
+
+  let node = root;
+  for (const el of arr) {
+    node.next = new ListNode(el);
+    node = node.next;
+  }
+
+  return root;
+}
