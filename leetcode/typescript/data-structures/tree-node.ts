22da05ad6fcc708c8ee9cf00ef1e7fb7d7c33836
Juan Carlos Lucio <c.lucio3@gmail.com>
2024-07-22 23:27:32 -0600

    Add createTree

--- leetcode/typescript/data-structures/tree-node.ts
+++ leetcode/typescript/data-structures/tree-node.ts
@@ -8,3 +8,27 @@ export class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+
+export function createTree(arr: (number | null)[]): TreeNode {
+  const root = new TreeNode(arr.shift() ?? 0);
+
+  const queue = [root];
+
+  while (queue.length > 0 && arr.length > 0) {
+    const current = queue.shift()!;
+
+    const leftVal = arr.shift();
+    const rightVal = arr.shift();
+
+    if (leftVal) {
+      current.left = new TreeNode(leftVal);
+      queue.push(current.left);
+    }
+    if (rightVal) {
+      current.right = new TreeNode(rightVal);
+      queue.push(current.right);
+    }
+  }
+
+  return root;
+}
