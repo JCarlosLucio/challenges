8cd25170df51ea7f821dcc61a3e043d5b8d99403
Juan Carlos Lucio <c.lucio3@gmail.com>
2025-05-30 00:20:13 -0600

    Fix formatting

--- leetcode/cpp/0001-two-sum.cpp
+++ leetcode/cpp/0001-two-sum.cpp
@@ -1,27 +1,22 @@
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 using std::unordered_map;
 using std::vector;
 
-class Solution
-{
+class Solution {
 public:
-  vector<int> twoSum(vector<int> &nums, int target)
-  {
-    unordered_map<int, int> numMap;
-    for (int i = 0; i < nums.size(); i++)
-    {
-      int num{nums[i]};
-      int complement{target - num};
-      if (numMap.count(complement))
-      {
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < static_cast<int>(nums.size()); i++) {
+            int num{nums[static_cast<size_t>(i)]};
+            int complement{target - num};
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[num] = i;
+        }
 
-        return {numMap[complement], i};
-      }
-      numMap[num] = i;
+        return {};
     }
-
-    return {};
-  }
 };
